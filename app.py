c1be37bdbbacc2cc631ca8dced1c1bb90d8d8247
Pranav722 <150027598+Pranav722@users.noreply.github.com>
2025-10-14 00:23:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from controllers.Resource_api import *
