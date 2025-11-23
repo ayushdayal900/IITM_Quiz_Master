e7fbaa4ba5b61dad7b095bc8032e8d7725da0153
ayush8 <ayushdayal900@gmail.com>
2025-11-23 23:40:51 +0530

    Adding complete project.

---
+++ app.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from flask import Flask
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from controllers.Resource_api import *
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = "1234567890!@#$%^&*()"
+
+login_manager = LoginManager(app)
+login_manager.login_view = "general_routes.login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User_Info.query.get(int(user_id))
+
+
+
+def init_blueprints():
+    # avoid circular import
+    from controllers.routes import gen, data, admin, user  
+
+    app.register_blueprint(gen)
+    app.register_blueprint(data)
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+
+from models.models import User_Info, db
+def initialize_database():
+    with app.app_context():
+        db.create_all() 
+        
+        admin_exists = User_Info.query.filter_by(id="0").first()
+        hashed_password = bcrypt.generate_password_hash("123").decode('utf-8')
+        if not admin_exists:
+            admin = User_Info(id="0", email="admin@iitm.ac.in", 
+                              password=hashed_password,  
+                              full_name="Admin Admin", qualification="12", 
+                              role=0, dob=datetime.strptime("2025-03-12", "%Y-%m-%d").date())
+            db.session.add(admin)
+            db.session.commit()
+            print("Admin user created successfully!")
+        else:
+            print("Admin user already exists.")
+
+
+db.init_app(app)  
+if __name__ == '__main__':
+    initialize_database()
+    init_blueprints()  
+    api.init_app(app)
+    app.run(debug=True)
---
+++ models/models.py
@@ -0,0 +1,124 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+
+class User_Info(db.Model, UserMixin):
+    __tablename__ = "user_info"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    full_name = db.Column(db.String, nullable=False)
+    qualification = db.Column(db.Integer, nullable=False)
+    role = db.Column(db.Integer, default=1)
+    dob = db.Column(db.Date, nullable=False)
+    user_score = db.Column(db.Integer, default=0)    
+
+    quizs = db.relationship("Quiz", cascade="all,delete", backref="user_info", lazy=True)
+
+
+
+
+
+
+
+
+
+
+
+class Subject(db.Model):
+    __tablename__ = "subject"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    
+    chaps = db.relationship("Chapter", cascade="all,delete", backref="subject", lazy=True)
+
+
+
+
+
+
+
+
+
+
+class Chapter(db.Model):
+
+    __tablename__ = "chapter"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    
+    ques = db.relationship("Question", cascade="all,delete", backref="chapter", lazy=True)
+    quizs = db.relationship("Quiz", cascade="all,delete", backref="chapter", lazy=True)
+
+
+
+
+
+
+
+
+
+
+
+class Question(db.Model):
+    __tablename__ = "question"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)  
+    description = db.Column(db.String, nullable=False)  
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    
+    option1 = db.Column(db.String, nullable=False)  
+    option2 = db.Column(db.String, nullable=False)  
+    option3 = db.Column(db.String, nullable=False)  
+    option4 = db.Column(db.String, nullable=False)  
+    correct_option = db.Column(db.Integer, nullable=False)  
+    
+
+
+
+
+
+
+
+
+    
+
+class Quiz(db.Model):
+    __tablename__ = "quiz"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_info.id'), nullable=False)
+    chapter_id = db.Column(db.Integer, db.ForeignKey('chapter.id'), nullable=False)
+
+    date_time = db.Column(db.DateTime, nullable=False)
+    duration = db.Column(db.Time, nullable=False)
+    #total score
+    score = db.Column(db.Integer, nullable=False)   
+    # is_attempted = db.Column(db.Boolean, default=False)
+    
+    quiz_maxm_score = db.Column(db.Integer, default=0)
+    questions = db.relationship("Question", cascade="all,delete",backref="quiz",lazy=True)
+
+
+
+
+
+
+
+
+
+
+class Score(db.Model):
+    __tablename__ = "score"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user_info.id'), nullable=False)
+    quiz_id = db.Column(db.Integer, db.ForeignKey('quiz.id'), nullable=False)
+    time_stamp_of_attempt = db.Column(db.DateTime, nullable=False)
+    # currect attempt score
+    score = db.Column(db.Integer, nullable=False)  
